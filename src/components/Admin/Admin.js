115a77c508025277180f3bebdeffd4d672987c4d
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-04-13 16:21:05 -0500

    basic admin tbl created, before moving to own comp

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,20 +1,58 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
 
 
 class Admin extends Component {
 
-render() {
-    return (
-        'hello from admin'
-    )
-}
+    handleDispatch = () => {
+        this.props.dispatch({ type: 'GET_PROJECTS' })
+    }
+
+    componentDidMount() {
+        this.handleDispatch();
+    }
+
+    render() {
+
+        return (
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            Name
+                        </TableCell>
+                        <TableCell>
+                            Action
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {this.props.reduxState.projects.map(project => 
+                    <TableRow key={project.id}>
+                          <TableCell>
+                            {project.name}      
+                        </TableCell>  
+                          <TableCell>
+                            <button>Delete</button>      
+                        </TableCell>  
+                    </TableRow>
+    
+                        
+                        )}
+                                    </TableBody>
+            </Table>
+        )
+    }
 
 }
 
 const mapReduxStateToProps = reduxState => ({
     reduxState
-  });
-  
-  export default connect(mapReduxStateToProps)(Admin);
-  
+});
+
+export default connect(mapReduxStateToProps)(Admin);
