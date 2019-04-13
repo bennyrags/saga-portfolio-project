a30f9b76905f5a03988e0b40ce416293b9fadebc
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-04-13 12:50:12 -0500

    added portfolioList as comp-mvd project list info there

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,57 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-import Grid from '@material-ui/core/Grid'
+import PortfolioList from '../PortfolioList/PortfolioList'
+
 class App extends Component {
 
-componentDidMount() {
-  this.props.dispatch({type:'GET_PROJECTS'})
-}
+
 
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-{this.props.reduxState.projects.map(project=>
-          
-      <Grid key={project.id} container spacing={16}>
-        <Grid className="border" item xs={3}>
-          <img className='thumbnail' src={project.thumbnail} alt={project.description} />
-        </Grid>
-        <Grid className="border" container item xs={9}>
-          <Grid className="border" item xs={3} >
-          <h3>{project.name}</h3>
-          </Grid>
-          <Grid className="border" item xs={3} >
-          <h4><a href={project.website}>Website</a></h4>
-          </Grid>
-          <Grid className="border" item xs={3} >
-          <h4><a href={project.github}>GitHub</a></h4>
-          </Grid>
-          <Grid className="border" item xs={3} >
-          <h6>{project.tag_name}</h6>
-          </Grid>
-          <Grid className="border" item xs={12} >
-          <p>{project.description}</p>
-          </Grid>
-
-        </Grid>
-        <Grid className="border" item>
-        <p>Grid 3</p>
-        </Grid>
-
-      </Grid>
-
-)}
-        
-      
+< PortfolioList /> 
       </div>
     );
   }
 }
 
 const mapReduxStateToProps = reduxState => ({
-reduxState,
+  reduxState,
 })
 
 export default connect(mapReduxStateToProps)(App);
---
+++ src/components/PortfolioList/PortfolioList.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+
+class PortfolioList extends Component {
+
+  //created this funct in case i need to pass as a prop to children comps
+  handleDispatch = () => {
+    this.props.dispatch({ type: 'GET_PROJECTS' })
+  }
+  
+    componentDidMount() {
+    this.handleDispatch();
+    }
+
+
+render() {
+    return(
+        <>
+        {this.props.reduxState.projects.map(project =>
+            <section key={project.id}>
+              <Grid container spacing={16}>
+                <Grid className="imgGrid" item xs={3}>
+                  <img className='thumbnail' src={project.thumbnail} alt={project.description} />
+                </Grid>
+                <Grid container item xs={9}>
+                  <Grid item xs={3} >
+                    <h3>{project.name}</h3>
+                  </Grid>
+                  <Grid item xs={3} >
+                    <h4><a href={project.website}>Website</a></h4>
+                  </Grid>
+                  <Grid item xs={3} >
+                    <h4><a href={project.github}>GitHub</a></h4>
+                  </Grid>
+                  <Grid item xs={3} >
+                    <h6>{project.tag_name}</h6>
+                  </Grid>
+                  <Grid item xs={12} >
+                    <p>{project.description}</p>
+                  </Grid>
+                </Grid>
+               
+              </Grid>
+              <Divider />
+            </section>
+          )}
+          </>
+    )
+}
+
+
+
+
+}
+
+const mapReduxStateToProps = reduxState => ({
+    reduxState,
+  })
+  
+  export default connect(mapReduxStateToProps)(PortfolioList);
